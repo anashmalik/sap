40d73e89180ce1c38858e4ce682da68392cd0e4d
Anash Malik <rocksme32@gmail.com>
2026-05-26 04:13:12 +0000

    25 may

---
+++ composite/webapp/controller/Main.controller.js
@@ -0,0 +1,22 @@
+ap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+
+    "use strict";
+
+    return Controller.extend(
+        "demo.comp.composite.controller.Main",
+        {
+
+            onInit: function () {
+
+                console.log(
+                    "Machine Dashboard Loaded"
+                );
+
+            }
+
+        }
+    );
+
+});
---
+++ composite/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["demo/co/composite/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
